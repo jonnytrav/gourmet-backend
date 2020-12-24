8cbd5dea4178cad2f2150ea26a0523436a2cf073
Jonathan Traverso <jtraverso7@gmail.com>
2020-12-24 16:15:36 -0500

    Established a port to which app will "listen".

--- server.js
+++ server.js
@@ -10,3 +10,14 @@ app.use(cors());
 // API
 const users = require("/api/users");
 app.use("/api/users", users);
+// NOT SURE IF I NEED NEXT FEW LINES
+app.use(express.static(path.join(__dirname, "../build")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../build"));
+});
+// **** END OF UNSURE CODE ****
+// port settings
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}. Should be 8000.`);
+});
